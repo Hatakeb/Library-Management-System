38ffec898f136820253573c935104e92fcdc1d45
unknown <Hatakeb>
2025-08-06 17:43:00 -0700

    Fix WhiteNoise middleware import

--- FinalYearProject/settings.py
+++ FinalYearProject/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware' # Add this below SecurityMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Add this below SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
